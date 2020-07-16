edae38edba5b0a3ba6abf7c2ef7488bda4de1686
jakubeeee <jakub.bereznicki@gmail.com>
2020-07-16 19:18:52 +0200

    Added a quicksort algorithm implementation

--- ms-java/src/main/java/com/jakubeeee/playground/msjava/algorithm/AlgorithmController.java
+++ ms-java/src/main/java/com/jakubeeee/playground/msjava/algorithm/AlgorithmController.java
@@ -15,9 +15,22 @@ public class AlgorithmController {
         this.algorithmService = algorithmService;
     }
 
+    // curl -X POST -d "iterations=1000" http://localhost:8080/algorithm/invokeloop
     @PostMapping(path = "algorithm/invokeloop")
     public void invokeLoop(@RequestParam(value = "iterations", defaultValue = "1000") int iterations) {
         algorithmService.invokeLoop(iterations);
+    }
+
+    // curl -X POST -d "amount=20" http://localhost:8080/algorithm/quicksortrandom
+    @PostMapping(path = "algorithm/quicksortrandom")
+    public void quickSortRandomInts(@RequestParam(value = "amount", defaultValue = "20") int amount) {
+        algorithmService.quickSortRandomInts(amount);
+    }
+
+    // curl -X POST -d "ints=1,10,2,9,3,8,4,7,5,6" http://localhost:8080/algorithm/quicksort
+    @PostMapping(path = "algorithm/quicksort")
+    public void quickSort(@RequestParam(value = "ints", defaultValue = "1,10,2,9,3,8,4,7,5,6") int[] unsortedInts) {
+        algorithmService.quickSort(unsortedInts);
     }
 
 }
--- ms-java/src/main/java/com/jakubeeee/playground/msjava/algorithm/AlgorithmService.java
+++ ms-java/src/main/java/com/jakubeeee/playground/msjava/algorithm/AlgorithmService.java
@@ -1,22 +1,85 @@
 package com.jakubeeee.playground.msjava.algorithm;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 @Service
 public class AlgorithmService {
 
+    /**
+     * Invokes a simple for loop.
+     *
+     * @param iterations the amount of loop iterations to perform
+     */
     public void invokeLoop(int iterations) {
+        if (iterations < 1) throw new IllegalArgumentException("Iteration amount must be positive");
         var startingInstant = Instant.now();
         for (int i = 1; i <= iterations; i++)
             LOGGER.info("Iteration number: {}", i);
         var endingInstant = Instant.now();
         var methodDuration = Duration.between(startingInstant, endingInstant);
         LOGGER.info("Invoke loop method duration: {}", methodDuration.toMillis());
+    }
+
+    /**
+     * Performs a quick sort of randomly generated int array.
+     *
+     * @param amount the size of the int array to sort
+     */
+    public void quickSortRandomInts(int amount) {
+        int[] unsortedInts = ThreadLocalRandom.current().ints(amount, 0, 100000).toArray();
+        quickSort(unsortedInts);
+    }
+
+    /**
+     * Performs a quick sort of int array.
+     *
+     * @param unsortedInts the unsorted int array
+     */
+    public void quickSort(@NonNull int[] unsortedInts) {
+        LOGGER.info("Array before applying quick sort: {}", unsortedInts);
+        var startingInstant = Instant.now();
+        int firstLowIndex = 0; // the index of the first array element
+        int firstHighIndex = unsortedInts.length - 1; // the index of the las array element
+        quickSortSubArray(unsortedInts, firstLowIndex, firstHighIndex);
+        var endingInstant = Instant.now();
+        var methodDuration = Duration.between(startingInstant, endingInstant);
+        LOGGER.info("Quick sort method duration: {}", methodDuration.toMillis());
+        LOGGER.info("Array after applying quick sort: {}", unsortedInts);
+    }
+
+    private void quickSortSubArray(int[] unsortedInts, int lowIndex, int highIndex) {
+        int middleIndex = lowIndex + (highIndex - lowIndex) / 2;
+        int middleInt = unsortedInts[middleIndex];
+        int leftSideIndex = lowIndex;
+        int rightSideIndex = highIndex;
+        while (leftSideIndex <= rightSideIndex) {
+            while (unsortedInts[leftSideIndex] < middleInt)
+                leftSideIndex++;
+            while (unsortedInts[rightSideIndex] > middleInt)
+                rightSideIndex--;
+            if (leftSideIndex <= rightSideIndex) {
+                swapArrayElements(unsortedInts, leftSideIndex, rightSideIndex);
+                leftSideIndex++;
+                rightSideIndex--;
+            }
+        }
+        if (lowIndex < rightSideIndex)
+            quickSortSubArray(unsortedInts, lowIndex, rightSideIndex);
+        if (highIndex > leftSideIndex)
+            quickSortSubArray(unsortedInts, leftSideIndex, highIndex);
+    }
+
+    private void swapArrayElements(int[] array, int firstIndex, int secondIndex) {
+        int firstElement = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = firstElement;
     }
 
 }
